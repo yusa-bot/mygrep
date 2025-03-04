8db17a10b14cb76c7aa96d9dbb9e6696276e8abf
yusa <yusaaihara@gmail.com>
2025-03-04 22:23:22 +0900

    3

--- mygrep.c
+++ mygrep.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-void search_file(const char *filename, const char *keyword)
+#define MAX_LINES 1000
+#define MAX_LINE_LENGTH 1024
+
+#ifndef _GNU_SOURCE
+char *my_strcasestr(const char *haystack, const char *needle)
+{
+    if (!*needle)
+        return (char *)haystack;
+
+    while (*haystack)
+    {
+        const char *h = haystack;
+        const char *n = needle;
+        while (*h && *n && tolower(*h) == tolower(*n))
+        {
+            h++;
+            n++;
+        }
+        if (!*n)
+            return (char *)haystack;
+        haystack++;
+    }
+    return NULL;
+}
+#define strcasestr my_strcasestr
+#endif
+
+void search_file(const char *filename, const char *keyword, int ignore_case)
 {
     FILE *file = fopen(filename, "r");
     if (!file)
@@ -12,9 +41,10 @@ void search_file(const char *filename, const char *keyword)
 
     char line[1024];
     int line_num = 1;
+
     while (fgets(line, sizeof(line), file))
     {
-        if (strstr(line, keyword))
+        if ((ignore_case && strcasestr(line, keyword)) || (!ignore_case && strstr(line, keyword)))
             printf("[%d] %s", line_num, line);
         line_num++;
     }
@@ -23,11 +53,25 @@ void search_file(const char *filename, const char *keyword)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
+    int ignore_case = 0;
+    char *keyword, *filename;
+
+    if (argc == 4 && strcmp(argv[1], "-i") == 0)
+    {
+        ignore_case = 1;
+        keyword = argv[2];
+        filename = argv[3];
+    }
+    else if (argc == 3)
+    {
+        keyword = argv[1];
+        filename = argv[2];
+    }
+    else
     {
         printf("使い方： mygrep <検索ワード> <ファイル名>\n");
         return 1;
     }
-    search_file(argv[2], argv[1]);
+    search_file(filename, keyword, ignore_case);
     return 0;
 }
